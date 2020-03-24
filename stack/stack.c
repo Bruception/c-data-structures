6dabe88ceb0a71dc9d1ee5d2311dc281db491ad5
Bruce Berrios <bberr022@fiu.edu>
2020-03-24 00:10:59 -0400

    Define create_stack

--- stack/stack.c
+++ stack/stack.c
@@ -3,3 +3,17 @@
 #include <stdlib.h>
 
 #include "stack.h"
+
+#define DEFAULT_CAPACITY 16
+
+stack* create_stack() {
+  stack* stk = (stack*)malloc(sizeof(stack));
+
+  stk->capacity = DEFAULT_CAPACITY;
+  stk->size = 0;
+
+  stk->data = (int*)malloc(sizeof(int) * DEFAULT_CAPACITY);
+  stk->top = stk->data;
+
+  return stk;
+}
