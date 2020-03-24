5ac40f02f67b65d094a49b1b37a3d97c4e188cdc
Bruce Berrios <bberr022@fiu.edu>
2020-03-23 21:19:46 -0400

    Define linkedlist header

---
+++ linkedlist/linkedlist.h
@@ -0,0 +1,23 @@
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct node_struct {
+  struct node_struct* next;
+  int value;
+
+} node;
+
+typedef struct linkedlist_struct {
+  node* head;
+  int size;
+
+} linkedlist;
+
+linkedlist* create_linkedlist();
+
+void add_linkedlist(linkedlist* list, int value);
+int search_linkedlist(linkedlist* list, int value);
+int delete_linkedlist(linkedlist* list, int value);
+
+#endif
