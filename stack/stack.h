534760f36a6a3e5fbc613e39ee67f9e705298d02
Bruce Berrios <bberr022@fiu.edu>
2020-03-24 00:10:38 -0400

    Add dump_stack to header

--- stack/stack.h
+++ stack/stack.h
@@ -6,7 +6,8 @@ typedef struct stack_struct {
   int size;
   int capacity;
 
-  int* stk;
+  int* data;
+  int* top;
 
 } stack;
 
@@ -15,5 +16,6 @@ stack* create_stack();
 int peek_stack(stack* stk);
 void push_stack(stack* stk);
 int pop_stack(stack* stk);
+void dump_stack(stack* stk);
 
 #endif
