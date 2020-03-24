26d2ad0a7b5bfdc300e4bcbfcabc7c1f74621fa9
Bruce Berrios <bberr022@fiu.edu>
2020-03-23 21:55:57 -0400

    Define add_linkedlist, refactor

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -4,20 +4,35 @@
 
 #include "linkedlist.h"
 
+node* create_node(int value, node* next) {
+  // Allocate space on the heap for node
+  node* newnode = (node*)malloc(sizeof(node));
+
+  newnode->value = value;
+  newnode->next = next;
+
+  return newnode;
+}
+
 linkedlist* create_linkedlist() {
   // Allocate space on the heap for linkedlist struct
   linkedlist* newlist = (linkedlist*)malloc(sizeof(linkedlist));
   newlist->size = 0;
 
-  // Allocate space on the heap for linkedlist head
-  node* head = (node*)malloc(sizeof(node));
-
-  head->next = NULL;
-  head->value = -1;
+  node* head = create_node(-1, NULL);
 
   newlist->head = head;
 
   return newlist;
+}
+
+void add_linkedlist(linkedlist* list, int value) {
+  node* oldhead = list->head->next;
+
+  node* newhead = create_node(value, oldhead);
+  list->head->next = newhead;
+
+  ++list->size;
 }
 
 void dump_linkedlist(linkedlist* list) {
@@ -25,10 +40,10 @@ void dump_linkedlist(linkedlist* list) {
 
   printf("(x) -> ");
 
-  node* current = list->head;
+  node* current = list->head->next;
 
-  while(current->next != NULL) {
-    printf("(%d) -> ", current->next->value);
+  while(current != NULL) {
+    printf("(%d) -> ", current->value);
     current = current->next;
   }
 
