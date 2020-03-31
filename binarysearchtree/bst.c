b912cd9ceddd634739a7bffbaf31c1b9a5ba5867
Bruce Berrios <bberr022@fiu.edu>
2020-03-31 01:16:01 -0400

    Add delete_bst and helper

--- binarysearchtree/bst.c
+++ binarysearchtree/bst.c
@@ -75,6 +75,41 @@ bst_node* min_node_bst(bst_node* root) {
   return current;
 }
 
+bst_node* delete_helper(bst* bt, bst_node* current, int value) {
+  if(current == NULL) return current;
+
+  if(value < current->value) {
+    current->left = delete_helper(bt, current->left, value);
+  } else if(value > current->value) {
+    current->right = delete_helper(bt, current->right, value);
+  } else {
+    if(current->left != NULL && current->right != NULL) {
+      bst_node* min_right_node = min_node_bst(current->right);
+
+      current->value = min_right_node->value;
+      current->right = delete_helper(bt, current->right, min_right_node->value);
+    } else {
+      bst_node* new_child = (current->left == NULL) ? (current->right) : (current->left);
+
+      free(current);
+      --bt->size;
+
+      return new_child;
+    }
+  }
+
+  return current;
+}
+
+int delete_bst(bst* bt, int value) {
+  if(bt->size == 0) exit(1);
+
+  int oldSize = bt->size;
+  bt->head = delete_helper(bt, bt->head, value);
+
+  return (oldSize > bt->size) ? (0) : (-1);
+}
+
 int max_bst(bst* bt) {
   if(bt->size == 0) exit(1);
 
